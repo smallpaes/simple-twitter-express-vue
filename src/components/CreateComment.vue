e4a80deb22b403d738c5897ea9def105ed17ffc8
Tao <ctaohe@gmail.com>
2019-09-28 12:33:41 +0800

    Change textarea

--- src/components/CreateComment.vue
+++ src/components/CreateComment.vue
@@ -1,12 +1,16 @@
 <template>
   <form @submit.stop.prevent="handleSubmit">
-    <div
-      @input="onInput"
-      class="p-3 mb-3 bg-light rounded"
-      style="min-height: 9em; border: 2px solid #1da1f2;"
-      contenteditable="true"
-      placeholder="What's on your mind?"
-    ></div>
+    <div class="test">
+      <textarea
+        @input="input"
+        v-model="description"
+        minlength="1"
+        maxlength="140"
+        class="textarea col-12 p-3 mb-3 bg-light rounded"
+        style="min-height: 9em;"
+        placeholder="What's on your mind?"
+      ></textarea>
+    </div>
     <span class="countText">尚可填寫 {{countText}} 字</span>
     <div class="text-right">
       <button type="submit" class="btn btn-primary rounded" :disabled="isProcessing">Tweet</button>
@@ -35,27 +39,26 @@ export default {
         UserId: this.UserId,
         description: this.description
       };
+      this.description = "";
       this.$emit("after-create-comment", formData);
+      this.isProcessing = true;
     },
-    onInput(e) {
-      let content = e.target.innerText;
-      this.countText = 140 - content.length;
-      if (content.length > 0 || content.length > 0) {
+    input(e) {
+      this.countText = 140 - e.target.value.length;
+      if (e.target.value.length > 0) {
         this.isProcessing = false;
+        e.target.style.cssText = `min-height: 9em;`
+        e.target.style.cssText = `min-height: ${e.target.scrollHeight}px;`
       } else {
         this.isProcessing = true;
       }
-      this.description = content
+      this.description = e.target.value;
     }
   }
 };
 </script>
 
 <style scoped>
-div[contenteditable]:empty:not(:focus):before {
-  content: attr(placeholder);
-  color: #aaa;
-}
 .countText {
   color: #aaa;
 }
